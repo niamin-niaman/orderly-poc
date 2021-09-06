9c069663202ec24f28829240461a63bca5486237
niamin-niaman <niamin.niaman@gmail.com>
2021-09-06 22:51:03 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-p56q24
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-p56q24)
---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import './style.css';
+
+import liff from '@line/liff';
+
+export default function App() {
+  const [name, setName] = useState('');
+
+  liff.ready.then(() => {
+    // do something you want when liff.init finishes
+    liff
+      .getProfile()
+      .then(profile => {
+        const name = profile.displayName;
+        setName(name);
+      })
+      .catch(err => {
+        console.log('err', err);
+      });
+  });
+
+  function initializeLiff(myLiffId) {
+    liff
+      .init({
+        liffId: myLiffId
+      })
+      .then(() => {
+        // start to use LIFF's api
+        console.log('success');
+        setName('Initailed. loading...');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  useEffect(() => {
+    initializeLiff('1656395790-8bWyddX5');
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello {name}! today</h1>
+    </div>
+  );
+}
