a9fa9c6ee57dba6c765b0858e40ee5b34c8ab1e5
niamin-niaman <niamin.niaman@gmail.com>
2021-09-20 21:45:00 +0700

    3 files modified

--- src/App.js
+++ src/App.js
@@ -4,44 +4,9 @@ import './style.css';
 import liff from '@line/liff';
 
 export default function App() {
-  const [name, setName] = useState('');
-
-  liff.ready.then(() => {
-    // do something you want when liff.init finishes
-    liff
-      .getProfile()
-      .then(profile => {
-        const name = profile.displayName;
-        // TODO call apollo here
-        setName(name);
-      })
-      .catch(err => {
-        console.log('err', err);
-      });
-  });
-
-  function initializeLiff(myLiffId) {
-    liff
-      .init({
-        liffId: myLiffId
-      })
-      .then(() => {
-        // start to use LIFF's api
-        console.log('success');
-        setName('Initailed. loading...');
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
-
-  useEffect(() => {
-    initializeLiff('1656395790-8bWyddX5');
-  }, []);
-
   return (
     <div>
-      <h1>Hello {name}! today</h1>
+      <h1>Hello! today</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql,
-} from '@apollo/client';
-
 import App from './App';
 
-const client = new ApolloClient({
-  uri: 'https://e729f1c3da7e02.localhost.run',
-  cache: new InMemoryCache(),
-});
+
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+    <App />,
   document.getElementById('root')
 );
