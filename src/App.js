6cea8152bfac57c3b71e31be25880a174dd924f0
niamin-niaman <niamin.niaman@gmail.com>
2021-09-21 20:53:03 +0700

    2 files modified

--- src/App.js
+++ src/App.js
@@ -4,9 +4,27 @@ import './style.css';
 import liff from '@line/liff';
 
 export default function App() {
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    setTimeout(() => {
+      setName('Ameen');
+    }, 1000);
+  }, []);
+  
   return (
     <div>
-      <h1>Hello! today</h1>
+      <h1>Hello! {name}today</h1>
+      <Test/>
     </div>
   );
 }
+
+const Test = () => {
+  // liff.init({ liffId: '1656395790-8bWyddX5' });
+  return (
+    <>
+      <h1>Test</h1>
+    </>
+  );
+};
